47484e2aded6676ca1e7fc263e576c6fae8d5c11
Wendy Romero <wrarquitecta@gmail.com>
2026-07-30 09:26:19 -0500

    feat: add if statement examples

---
+++ src/main/java/com/wendyromero/fundamentals/IfExample.java
@@ -0,0 +1,16 @@
+package com.wendyromero.fundamentals;
+
+public class IfExample {
+    public static void main(String[] args) {
+
+        int edad = 17;
+
+        if (edad >= 18) {
+            System.out.println("Eres mayor de edad.");
+                    System.out.println("Puedes ingresar.");
+
+    }   System.out.println("Fin del programa.");
+
+}
+
+}
